aea0d94e5693e0be51513baac54bae7587c389bc
Mike <mike@mikeowino.com>
2022-01-26 19:15:06 +0300

    Create script.py

---
+++ script.py
@@ -0,0 +1,205 @@
+! 2021-12-19 https://coinmarketcap.com
+coinmarketcap.com##.jiWaSe.ii1g3i-0 > .ivGjzC.mhbkgs-0 > .carousel-items-wrapper
+coinmarketcap.com##.gRdZsE.sc-1o3rzds-1.sc-1o3rzds-0
+
+! 2021-12-19 https://www.coingecko.com
+www.coingecko.com##.tw-my-4 > .tw-items-center.tw-flex-row.tw-flex
+www.coingecko.com##.dark\:tw-text-gray-400.tw-mt-1.tw-text-gray-500.tw-text-sm
+
+! 2021-12-19 https://autochek.africa
+||www.google.com/recaptcha/enterprise/anchor?ar=1&k=6Lc1C6waAAAAAB4EfwtKfbNmsU3umH5YYd5h3bey&co=aHR0cHM6Ly9hdXRvY2hlay5hZnJpY2E6NDQz&hl=en&v=VZKEDW9wslPbEc9RmzMqaOAP&size=invisible&cb=fu0wh0d9g80n$subdocument
+||www.google.com/recaptcha/enterprise/anchor?ar=1&k=6Lc1C6waAAAAAB4EfwtKfbNmsU3umH5YYd5h3bey&co=aHR0cHM6Ly9hdXRvY2hlay5hZnJpY2E6NDQz&hl=en&v=VZKEDW9wslPbEc9RmzMqaOAP&size=invisible&cb=fu0wh0d9g80n$subdocument
+
+! 2021-12-19 https://app.netlify.com
+app.netlify.com##.tw-w-auto.btn-primary--gold.btn-primary.btn-default.btn
+
+! 2021-12-19 https://vercel.com
+vercel.com##.feedback-input_placeholder__ZKdJ6
+vercel.com##.feedback-input_blur__v3I6x.feedback-input_feedback-wrapper__mCwSO
+vercel.com##.feedback-input_geist-feedback-input__PxEnJ
+vercel.com##.geist-secondary-fill.geist-secondary.geist-themed.badge_small__Kpl__.badge_badge__LNH0p
+
+! 2021-12-19 https://www.seedr.cc
+www.seedr.cc##body > footer
+www.seedr.cc##.text
+www.seedr.cc##.account-name
+
+! 2021-12-19 https://www.1337x.to
+||www.1337x.to/css/images/main-bg.png$image
+||www.1337x.to/css/images/main-bg.png$image
+
+! 2021-12-20 https://decrypt.co
+decrypt.co##.jRijbv.jkxCRA.sc-xt8j06-3.sc-1ao5pjp-0
+
+! 2021-12-21 https://pixlr.com
+||pixlr.com/dad/cut/$subdocument
+||pixlr.com/dad/cut/$subdocument
+
+! 2021-12-21 https://www.jumia.co.ke
+||ke.jumia.is/cms/2021/WK50/Revision/KE_Cross_KrisNaJumia_JExpress_1121_STB-(1).gif$image
+www.jumia.co.ke##div.banner
+
+! 2021-12-21 https://www.youtube.com
+||www.youtube.com/live_chat?continuation=0ofMyANxGlhDaWtxSndvWVZVTllOazlSTTBSclkzTmlXVTVGTmtnNGRWRlJkVlpCRWd0QlNURnpYMnhFVFU5R1JSb1Q2cWpkdVFFTkNndEJTVEZ6WDJ4RVRVOUdSU0FCMAGCAQIIBIgBAaAB1aXKqcn19AKyAQA%253D$subdocument
+||www.youtube.com/live_chat?continuation=0ofMyANxGlhDaWtxSndvWVZVTllOazlSTTBSclkzTmlXVTVGTmtnNGRWRlJkVlpCRWd0QlNURnpYMnhFVFU5R1JSb1Q2cWpkdVFFTkNndEJTVEZ6WDJ4RVRVOUdSU0FCMAGCAQIIBIgBAaAB1aXKqcn19AKyAQA%253D$subdocument
+||www.youtube.com/live_chat?continuation=0ofMyANxGlhDaWtxSndvWVZVTllOazlSTTBSclkzTmlXVTVGTmtnNGRWRlJkVlpCRWd0QlNURnpYMnhFVFU5R1JSb1Q2cWpkdVFFTkNndEJTVEZ6WDJ4RVRVOUdSU0FCMAGCAQIIBIgBAaAB1aXKqcn19AKyAQA%253D$subdocument
+www.youtube.com##.ytd-live-chat-frame.style-scope > .ytd-toggle-button-renderer.style-scope.yt-simple-endpoint > .ytd-toggle-button-renderer.style-scope
+www.youtube.com###show-hide-button
+www.youtube.com###chat
+
+! 2021-12-22 https://www.seedr.cc
+||static.seedr.cc/images/avatars/farmer_new.png$image
+
+! 2021-12-23 https://www.jumia.co.ke
+www.jumia.co.ke##.-gy3.-bg-gy75.-fs12.-pbm
+||ke.jumia.is/cms/2021/W49/HP/Banners/KE_Cross_KrisiNaJumia_Generic_1221_Takeover.jpg$image
+||ke.jumia.is/cms/2021/W49/HP/Banners/KE_Cross_KrisiNaJumia_Generic_1221_Takeover.jpg$image
+
+! 2021-12-23 https://pay.jumia.co.ke
+pay.jumia.co.ke##.primary.theme--dark.v-footer
+pay.jumia.co.ke##.secondary.theme--dark.v-footer
+
+! 2021-12-23 https://www.freepik.com
+www.freepik.com##[href^="https://clk.tradedoubler.com/click"] > picture
+www.freepik.com##.astock
+www.freepik.com##.alignc.container-fluid
+www.freepik.com##div.spr--fixed-height.spr:nth-of-type(13)
+www.freepik.com##.price_before.banner-container.banner-detail.banner-campaign
+www.freepik.com##.price_before.banner-upper.banner-search.banner-campaign
+www.freepik.com##.pd-x-none.pd-lv3.footer__top
+www.freepik.com###footer
+
+! 2021-12-28 https://open.spotify.com
+open.spotify.com##.Upqw01TOXETOmR5Td7Dj.X7lQWw2Ly_RZVPEFj2QY.Qt5xfSWikz6CLU8Vobxs
+open.spotify.com##.WvLkmOVB2R2vzI2ibR_r.eNs6P3JYpf2LScgTDHc6 > .ATUzFKub89lzvkmvhpyE.link-subtle
+
+! 2021-12-28 https://www.binance.com
+www.binance.com##.css-1cep7gu.trade-carousel-vertical.trade-carousel
+www.binance.com##.css-1g989cj
+
+! 2021-12-28 https://www.freepik.com
+www.freepik.com##div.spr--internal.spr--fixed-height.spr:nth-of-type(11)
+
+! 2021-12-28 https://www.youtube.com
+www.youtube.com##.ytd-watch-flexy.style-scope > .ytd-donation-unavailable-renderer.style-scope
+
+! 2021-12-28 https://www.freepik.com
+www.freepik.com##.percent_off.banner-upper.banner-search.banner-campaign
+
+! 2021-12-28 https://www.seedr.cc
+www.seedr.cc##.swal2-show.swal2-icon-warning.swal2-toast.swal2-popup
+
+! 2021-12-28 https://pngtree.com
+pngtree.com##.xs-hide._ncp-header.stylus-banner-top.stylus-banner > .hue-js.stylus-link
+pngtree.com##.side-group
+
+! 2021-12-28 https://www.photopea.com
+www.photopea.com##.app.flexrow > div
+
+! 2021-12-30 https://github.com
+github.com##.flex-md-order-none.flex-order-1.mr-md-3.mr-0.Header-item
+
+! 2021-12-31 https://dashboard.heroku.com
+dashboard.heroku.com##.no-underline.items-center.flex.pl3-ns.pl2.b--light-silver.bb.glostick-helmet-link
+
+! 2022-01-02 https://www.youtube.com
+www.youtube.com##.style-default.ytd-topbar-menu-button-renderer.style-scope
+www.youtube.com##.notification-button-style-type-default.ytd-notification-topbar-button-renderer.style-scope
+www.youtube.com###voice-search-button > .size-default.style-default.ytd-masthead.style-scope > .ytd-button-renderer.style-scope.yt-simple-endpoint > .size-default.style-default.ytd-button-renderer.style-scope
+www.youtube.com###voice-search-button
+www.youtube.com###clarify-box > .ytd-watch-flexy.style-scope
+
+! 2022-01-02 https://web.postman.co
+web.postman.co##.requester-header__team-info__button.team-button.btn-secondary.btn
+web.postman.co##.workspace-invite-btn.btn-small.btn-secondary.btn
+
+! 2022-01-03 https://1337x.to
+||1337x.to/images/logo.svg$image
+||1337x.to/images/logo.svg$image
+1337x.to##.clearfix.box-info-heading
+1337x.to##.clearfix.box-info-detail
+1337x.to##footer
+1337x.to##.top-bar
+1337x.to##.pull-right.col-3
+1337x.to##[href="/popular-xxx"]
+1337x.to##[href="/popular-documentaries"]
+
+! 2022-01-03 https://www.youtube.com
+www.youtube.com##.notification-button-style-type-default.ytd-masthead.style-scope
+
+! 2022-01-03 https://1337x.to
+||1337x.to/css/images/main-bg.png$image
+
+! 2022-01-03 https://www.goojara.to
+www.goojara.to##.live
+www.goojara.to##footer
+
+! 2022-01-03 https://www.torlock.com
+||l.tcdn.fun/no-cover.png$image
+||l.tcdn.fun/no-cover.png$image
+www.torlock.com##.frontcloud
+
+! 2022-01-03 https://hecoinfo.com
+||s2.loli.net/2021/12/29/iDYuvsjGxph9rR6.jpg$image
+||s2.loli.net/2021/12/29/iDYuvsjGxph9rR6.jpg$image
+||s2.loli.net/2021/12/08/S6YX9JiH5eLlvIQ.jpg$image
+||s2.loli.net/2021/12/08/S6YX9JiH5eLlvIQ.jpg$image
+hecoinfo.com##.col-lg-5.col-12
+
+! 2022-01-03 https://polygonscan.com
+polygonscan.com###ContentPlaceHolder1_divSummary > .justify-content-center.d-flex
+polygonscan.com##.pl-xll-5.pl-lg-4.text-center.d-lg-block.d-none
+
+! 2022-01-08 https://replit.com
+replit.com##.css-1u6rog0.floating-icon-wrapper.jsx-26301316
+
+! 2022-01-08 https://1337x.to
+1337x.to##[href="/popular-anime"]
+
+! 2022-01-08 https://www.seedr.cc
+www.seedr.cc###ultra-top
+
+! 2022-01-10 https://www.bloomberg.com
+www.bloomberg.com##._2jge3GYu2lbNda7WGvmA86._1XooH4tzsM-atQfXfXrgGX
+
+! 2022-01-10 https://replit.com
+replit.com##.logo-link.jsx-3980429440 > .jsx-4131126525
+replit.com##.css-1eirrsc
+replit.com##.help-container.jsx-1557842061 > .sidebar-help.jsx-3162959471 > .help-button.jsx-3162959471 > .css-qrpwoq
+replit.com##.help-container.jsx-1557842061
+
+! 2022-01-10 https://twitter.com
+twitter.com##.r-qo02w8.r-l5o3uw.css-1dbjc4n
+
+! 2022-01-12 https://github.com
+github.com##.mt-md-0.mt-2.flex-md-order-none.flex-order-1 > .mb-3
+
+! 2022-01-12 https://replit.com
+replit.com##.logo-link.jsx-3980429440
+
+! 2022-01-16 https://pixlr.com
+pixlr.com###right-space
+||pixlr.com/dad/pop/$subdocument
+||pixlr.com/dad/pop/$subdocument
+||pixlr.com/dad/pop/$subdocument
+pixlr.com##.pad-20.flatbox
+
+! 2022-01-17 http://getrevue.bloodgoodbtc.com
+getrevue.bloodgoodbtc.com###subscribe-bar
+
+! 2022-01-21 https://www.netflix.com
+www.netflix.com##.error.message
+
+! 2022-01-22 https://api.whatsapp.com
+api.whatsapp.com##._8ibq._2yyk > ._2z07._2z0c._2y_c._36or
+api.whatsapp.com##._8ibx
+
+! 2022-01-24 https://www.tipranks.com
+www.tipranks.com##.ipadpro_displaynone.ml3.whiteSpacenowrap.hoverBgorange.laptop_fontSize9.laptop_lineHeight1.mx4.flexrcc.fontSize7.pxmedium4.fontFamilyregular.fontWeightsemibold.borderColororange-light.bgorange-light.radiiround.h_pxsmall1.colorwhite
+www.tipranks.com##.displayflex._3rFk6QFilyZfk2qzHGCMZJ > .displayflex.flexrec
+www.tipranks.com##.mb5.ipadpro_fontSize10.ipad_displaynone.bgAlpha10.shadow1.colorblack.borderColororange.border1.displayflex.px3.p2.flexrsc.bgorange
+
+! 2022-01-26 https://www.youtube.com
+www.youtube.com##.ytd-badge-supported-renderer.style-scope.badge-style-type-verified.badge > yt-icon.ytd-badge-supported-renderer.style-scope
+www.youtube.com###country-code
+
